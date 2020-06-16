250e1f727c3ec48450567c18f9062a4f8ec1bb82
mxchacha <mxchacha@espe.edu.ec>
2020-06-16 16:29:23 -0500

    TEMPLATE

---
+++ src/main/java/com/chachalopez/PryCertificacion/controllers/MainController.java
@@ -0,0 +1,29 @@
+package com.chachalopez.PryCertificacion.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping(value= {"/","/index.html"})
+public class MainController {
+	
+	@GetMapping(value="/")
+	  public String home(Model model) {
+		  
+		  return "index";/*Ubicación de la vista*/
+	  }
+	
+	@GetMapping(value="/tables.html")
+	public String tables(Model model) {						
+		return "tables";
+	}
+	
+	@GetMapping(value="/register.html")
+	public String register(Model model) {						
+		return "register";
+	}
+
+}

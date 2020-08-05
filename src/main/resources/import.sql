a5169a8794d22780ad89f88149fa9cfed0568c68
Klopez-5 <kalopez5@espe.edu.ec>
2020-08-05 01:08:42 -0500

    Complemento de seguridades

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,6 @@
+INSERT INTO  tipocuentas(pk_tipocuenta, descripcion,nombre) VALUES(1,'Cuenta de ahorro a futuro','Cuenta de Ahorro');
+INSERT INTO  tipocuentas(pk_tipocuenta, descripcion,nombre) VALUES(2,'Cuenta de movimiento constante','Cuenta Corriente');
 
+INSERT INTO  tipoprestamos(pk_tipoprestamo, interes,nombre, plazo) VALUES(1,12,'Plazo 1','Plazo a 12 meses');
+INSERT INTO  tipoprestamos(pk_tipoprestamo, interes,nombre, plazo) VALUES(2,6,'Plazo 2','Plazo a 6 meses');
+INSERT INTO  tipoprestamos(pk_tipoprestamo, interes,nombre, plazo) VALUES(3,3,'Plazo 3','Plazo a 3 meses');
